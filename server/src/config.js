b1fce0520e7df31d7c6108bef725d95adb83aea0
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2018-12-14 22:52:16 +0800

    Update config.js

--- server/src/config.js
+++ server/src/config.js
@@ -3,10 +3,10 @@
 const path = require('path')
 
 module.exports = {
-  db: 'mongodb://localhost/',
-  log: path.resolve(__dirname, '../logs/board.log'),
+  db: 'mongodb://127.0.0.1:27017/',
+  log: path.resolve(__dirname, '../../logs/board.log'),
   base: 'board',
   port: '3002',
   root: 'http://iszu.cn',
-  secret: 'buzhidao'
+  secret: ''
 }
