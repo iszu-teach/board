6057b31a18b11a401384ebbe3a492a8988277525
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2019-03-27 19:59:56 +0800

    feat: update config

--- server/src/config.js
+++ server/src/config.js
@@ -3,10 +3,10 @@
 const path = require('path')
 
 module.exports = {
-  db: 'mongodb://szuwechat:szu0408@localhost:28018/',
+  db: '',
   log: path.resolve(__dirname, '../../../logs/board.log'),
   base: 'board',
   port: '3002',
   root: 'http://szuwechat.cn',
-  secret: 'szu0408'
+  secret: ''
 }
